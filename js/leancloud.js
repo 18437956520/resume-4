49efeaeafd761c991f7da4d2e2367342c732bd71
yyyh <444510796@qq.com>
2019-04-18 15:40:40 +0800

    message

--- js/leancloud.js
+++ js/leancloud.js
@@ -2,8 +2,8 @@ var APP_ID = 'HCvcsO4BR07NaFVLHsiEu1gW-gzGzoHsz';
 var APP_KEY = 'EfobIcGHRYQ8iBflM5KAVrro';
 
 AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
+    appId: APP_ID,
+    appKey: APP_KEY
 });
 
 // 创建 TestObject 表 
@@ -13,7 +13,7 @@ var testObject = new TestObject();
 // 数据内容为 words: 'Hello World!' 保存
 // 保存成功 则运行 alert('')
 testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
+    words: 'Hello World!'
+}).then(function (object) {
+    alert('LeanCloud Rocks!');
 })
