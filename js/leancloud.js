0f3382c9926fae871b99a92531d908bcfd648f1a
yyyh <444510796@qq.com>
2019-04-18 16:06:39 +0800

    留言存入

--- js/leancloud.js
+++ js/leancloud.js
@@ -6,6 +6,21 @@ AV.init({
     appKey: APP_KEY
 });
 
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let content = myForm.querySelector('input[name=content]').value
+    var Message = AV.Object.extend('Message')
+    var message = new Message()
+    message.save({
+        'content': content
+    }).then(function (onject) {
+
+    })
+})
+
+/*example
 // 创建 TestObject 表 
 var TestObject = AV.Object.extend('TestObject');
 // 在表中创建一行数据
@@ -15,5 +30,6 @@ var testObject = new TestObject();
 testObject.save({
     words: 'Hello World!'
 }).then(function (object) {
-    alert('LeanCloud Rocks!');
+    
 })
+*/
