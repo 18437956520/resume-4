d9975407f7f3f4a8e5e4aadb148d0b21dbc28812
yyyh <444510796@qq.com>
2019-04-18 18:34:03 +0800

    留言mvc完成

--- js/leancloud.js
+++ js/leancloud.js
@@ -1,54 +1,62 @@
-var APP_ID = 'HCvcsO4BR07NaFVLHsiEu1gW-gzGzoHsz';
-var APP_KEY = 'EfobIcGHRYQ8iBflM5KAVrro';
+! function () {
+    var view = document.querySelector('section.message')
 
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-
-var query = new AV.Query('Message');
-query.find().then(
-    function (messages) {
-        let array = messages.map((item) => item.attributes)
-        array.forEach((item) => {
-            let li = document.createElement('li')
-            li.innerText = `${item.name}: ${item.content}`
-            let messageList = document.querySelector('#messageList')
-            messageList.append(li)
-        })
+    var controller = {
+        view: null,
+        messageList: null,
+        init: function (view) {
+            this.view = view
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('form')
+            this.initAV()
+            this.loadMessages()
+            this.bindEvents()
+        },
+        initAV: function () {
+            var APP_ID = 'HCvcsO4BR07NaFVLHsiEu1gW-gzGzoHsz';
+            var APP_KEY = 'EfobIcGHRYQ8iBflM5KAVrro';
+            AV.init({
+                appId: APP_ID,
+                appKey: APP_KEY
+            })
+            console.log(1)
+        },
+        loadMessages: function () {
+            var query = new AV.Query('Message');
+            query.find().then(
+                (messages) => {
+                    let array = messages.map((item) => item.attributes)
+                    array.forEach((item) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${item.name}: ${item.content}`
+                        this.messageList.append(li)
+                    })
+                }
+            )
+            console.log(2)
+        },
+        bindEvents: function () {
+            let myForm = this.form
+            myForm.addEventListener('submit', function (e) {
+                e.preventDefault()
+                let content = myForm.querySelector('input[name=content]').value
+                let name = myForm.querySelector('input[name=name]').value
+                var Message = AV.Object.extend('Message')
+                var message = new Message()
+                message.save({
+                    'name': name,
+                    'content': content
+                }).then(function (object) {
+                    let li = document.createElement('li')
+                    li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+                    let messageList = document.querySelector('#messageList')
+                    messageList.appendChild(li)
+                    myForm.querySelector('input[name=content]').value = ''
+                })
+                console.log(4)
+            })
+            console.log(3)
+        },
     }
-)   
-
-let myForm = document.querySelector('#postMessageForm')
-
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    let content = myForm.querySelector('input[name=content]').value
-    let name = myForm.querySelector('input[name=name]').value
-    var Message = AV.Object.extend('Message')
-    var message = new Message()
-    message.save({
-        'name': name,
-        'content': content
-    }).then(function (object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        let messageList = document.querySelector('#messageList')
-        messageList.append(li)
-        myForm.querySelector('input[name=content]').value = ''
-    })
-})
-
-/*example
-// 创建 TestObject 表 
-var TestObject = AV.Object.extend('TestObject');
-// 在表中创建一行数据
-var testObject = new TestObject();
-// 数据内容为 words: 'Hello World!' 保存
-// 保存成功 则运行 alert('')
-testObject.save({
-    words: 'Hello World!'
-}).then(function (object) {
-    
-})
-*/
+    controller.init(view)
+}.call()
